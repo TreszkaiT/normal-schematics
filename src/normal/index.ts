dd12d14db10f82cb239a5fc7b6b75ceabedc845b
TreszkaiT <treszkaitom@gmail.com>
2023-06-07 21:47:22 +0200

    fix: normal index.ts

--- src/normal/index.ts
+++ src/normal/index.ts
@@ -1,15 +1,3 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function normal(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
-}
-
-
 import {
   chain,
   Rule,
